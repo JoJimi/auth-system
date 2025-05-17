9ecd40b8187e9c77a0e5ef1343112dcd08bb3c73
duddnddnfl <duddnddnfl@naver.com>
2025-05-17 22:11:42 +0900

    init project

---
+++ src/test/java/org/example/auth_system/AuthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.auth_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
