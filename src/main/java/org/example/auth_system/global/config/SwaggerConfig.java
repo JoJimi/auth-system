1c2776f695723a03fa74a2745c0744508d2c13ce
duddnddnfl <duddnddnfl@tukorea.ac.kr>
2025-05-21 16:11:34 +0900

    swagger 부분 authentification 설정

--- src/main/java/org/example/auth_system/global/config/SwaggerConfig.java
+++ src/main/java/org/example/auth_system/global/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package org.example.auth_system.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +14,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI(){
         return new OpenAPI()
-                .components(new Components())
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentification"))
+                .components(new Components().addSecuritySchemes("Bearer Authentification", createAPIKeyScheme()))
                 .info(apiInfo());
     }
 
@@ -21,6 +24,12 @@ public class SwaggerConfig {
                 .title("통합 인증 프로젝트")
                 .description("Spring doc를 사용한 auth system swagger UI")
                 .version("1.0.0");
+    }
+
+    private SecurityScheme createAPIKeyScheme(){
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 
 }
