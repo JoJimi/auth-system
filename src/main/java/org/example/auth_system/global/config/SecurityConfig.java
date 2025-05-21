bc3124b9fa72612d00d2a65c47a6d0aac7b6b1b5
duddnddnfl <duddnddnfl@tukorea.ac.kr>
2025-05-21 16:10:28 +0900

    간단한 security 설정

---
+++ src/main/java/org/example/auth_system/global/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package org.example.auth_system.global.config;
+
+import org.example.auth_system.global.security.filter.JwtAuthFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String[] AUTH_ALLOWLIST = {
+        "/swagger-ui/**",
+        "/v3/**"
+    };
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(form -> form.disable())
+                .addFilterBefore(new JwtAuthFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(AUTH_ALLOWLIST).permitAll()
+                        .anyRequest().authenticated())
+                .build();
+
+    }
+
+}
