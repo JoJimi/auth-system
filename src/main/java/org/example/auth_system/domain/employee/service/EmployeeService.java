2aa61b7d38652d5085ec4f733a0e135ffd1c156e
duddnddnfl <duddnddnfl@tukorea.ac.kr>
2025-05-21 16:43:49 +0900

    로그 추가

--- src/main/java/org/example/auth_system/domain/employee/service/EmployeeService.java
+++ src/main/java/org/example/auth_system/domain/employee/service/EmployeeService.java
@@ -3,11 +3,13 @@ package org.example.auth_system.domain.employee.service;
 import com.sun.jdi.request.DuplicateRequestException;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.auth_system.domain.department.entity.Department;
 import org.example.auth_system.domain.department.repository.DepartmentRepository;
 import org.example.auth_system.domain.employee.dto.response.EmployeeResponse;
 import org.example.auth_system.domain.employee.entity.Employee;
 import org.example.auth_system.domain.employee.repository.EmployeeRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,12 +17,15 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
     private  final DepartmentRepository departmentRepository;
 
     public List<EmployeeResponse> listEmployees(){
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("Security Context Holder name: " + name);
         return employeeRepository.findAll().stream()
                 .map(EmployeeResponse::from)
                 .collect(Collectors.toList());
